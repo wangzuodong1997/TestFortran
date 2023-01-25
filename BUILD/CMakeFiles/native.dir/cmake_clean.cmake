dfa69bd7044d4572cb6c1d8ea0565c52b1c31347
Zuodong Wang <frwangzuodong@163.com>
2023-01-25 14:02:57 +0100

    wzd

--- BUILD/CMakeFiles/native.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/native"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/native.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
